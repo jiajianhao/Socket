39e515a051c83248e4b779d89f32184addcf6b4b
贾建昊 <980497452@qq.com>
2017-08-16 20:08:29 +0800

    update socketserver and socketclient

--- ServerClient/ServerClient/ViewController.m
+++ ServerClient/ServerClient/ViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <GCDAsyncSocket.h>
+#define mDefaultHost @"192.168.31.76"
+#define mDefaultPort 8889
 
-@interface ViewController ()
-
+@interface ViewController ()<GCDAsyncSocketDelegate>
+//客户端socket
+@property (nonatomic) GCDAsyncSocket *clientSocket;
 @end
 
 @implementation ViewController
@@ -23,6 +27,69 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+#pragma mark -- Private Method
+
+/**
+ 发起连接
+ */
+-(IBAction)startConnect:(id)sender{
+    [self.clientSocket connectToHost:mDefaultHost onPort:mDefaultPort withTimeout:-1 error:nil];
+}
+/**
+ 发送消息
+ */
+-(IBAction)sendMessage:(id)sender{
+    NSString *newMsg = [NSString stringWithFormat:@"This is Client ! %@",[NSDate date]];
+    NSData *data = [newMsg dataUsingEncoding:NSUTF8StringEncoding];
+    [self.clientSocket writeData:data withTimeout:-1 tag:0];
+    
+}
+
+/**
+ 接收消息
+*/
+-(IBAction)receiveMessage:(id)sender{
+    [self.clientSocket readDataWithTimeout:5 tag:0];
+
+}
+#pragma mark -- GCDAsyncSocket Deleagte
+-(void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port{
+    NSLog(@"连接上%@",host);
+    
+    [self.clientSocket readDataWithTimeout:-1 tag:0];
+
+    
+}
+
+-(void)socket:(GCDAsyncSocket *)sock didAcceptNewSocket:(GCDAsyncSocket *)newSocket{
+    self.clientSocket = newSocket;
+    NSLog(@"连接成功");
+    NSLog(@"didAcceptNewSocket:-- %@:-%hu",newSocket.connectedHost,newSocket.connectedPort);
+    [self.clientSocket readDataWithTimeout:-1 tag:0];
+    
+}
+
+-(void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag{
+    NSString *text = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"didReadData: --  %@",text);
+    
+    [self.clientSocket readDataWithTimeout:-1 tag:0];
+    
+}
+
+-(void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)err{
+    NSLog(@"socketDidDisconnect");
+    
+}
+
+#pragma mark -- Setter/Getter
+-(GCDAsyncSocket *)clientSocket{
+    if (!_clientSocket) {
+        _clientSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+
+    }
+    return _clientSocket;
 }
 
 
--- SocketServer/SocketServer/ViewController.m
+++ SocketServer/SocketServer/ViewController.m
@@ -7,16 +7,22 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import <GCDAsyncSocket.h>
+#define mDefaultPort 8889
+@interface ViewController ()<GCDAsyncSocketDelegate>
+//服务器socket
+@property (nonatomic) GCDAsyncSocket *serverSocket;
+//客户端socket
+@property (nonatomic) GCDAsyncSocket *clientSocket;
 @end
 
 @implementation ViewController
-
+#pragma mark -- Life Cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+   
+    [self beginObserver];
 }
 
 
@@ -24,6 +30,70 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark -- Private Method
+
+/**
+ 开始监听
+ */
+-(void)beginObserver{
+    NSError *error = nil;
+    BOOL result = [self.serverSocket acceptOnPort:mDefaultPort error:&error];
+    if (result && error == nil) {
+        NSLog(@"开始监听网络");
+    }
+
+}
+
+/**
+ 发送消息
+ */
+-(void)sendMessage{
+    NSString *newMsg = [NSString stringWithFormat:@"This is Server ! %@",[NSDate date]];
+    NSData *data = [newMsg dataUsingEncoding:NSUTF8StringEncoding];
+    [self.clientSocket writeData:data withTimeout:-1 tag:0];
+    
+}
+#pragma mark -- GCDAsyncSocket Deleagte
+-(void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port{
+    NSLog(@"连接上%@",host);
+    
+}
+
+-(void)socket:(GCDAsyncSocket *)sock didAcceptNewSocket:(GCDAsyncSocket *)newSocket{
+    self.clientSocket = newSocket;
+    NSLog(@"连接成功");
+    NSLog(@"didAcceptNewSocket:-- %@:-%hu",newSocket.connectedHost,newSocket.connectedPort);
+    [self.clientSocket readDataWithTimeout:-1 tag:0];
+
+}
+
+-(void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag{
+    NSString *text = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"didReadData: --  %@",text);
+    
+    [self.clientSocket readDataWithTimeout:-1 tag:0];
+
+    [self sendMessage];
+}
+
+-(void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)err{
+    NSLog(@"socketDidDisconnect");
+    
+}
+
+#pragma mark -- Setter/Getter
+-(GCDAsyncSocket *)serverSocket{
+    if (!_serverSocket) {
+        _serverSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+    }
+    return _serverSocket;
+}
+
+
+
+
+
+
 
 
 @end
